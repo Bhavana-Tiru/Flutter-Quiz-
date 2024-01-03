bd9df35b87623968fb8eed26bedb538f7dd47780
Bhavana <tiruveedulabhavana96@gmail.com>
2024-01-04 02:35:38 +0530

    Added new dart files

---
+++ adv_basics/lib/models/quiz_qusetion.dart
@@ -0,0 +1,7 @@
+// dart file that containes question blue print
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers); //Constructor fun
+
+  final String text;
+  final List<String> answers;
+}
--- adv_basics/lib/question.dart
+++ adv_basics/lib/question.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/answer_button.dart';
 
 class Question extends StatefulWidget {
   const Question({super.key});
@@ -12,6 +13,36 @@ class Question extends StatefulWidget {
 class _Question extends State<Question> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Quiz Starts');
+    return SizedBox(
+      width:
+          double.infinity, //to use as width as you can or as wide as possible
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //const- stored in the menory once and can reuse by dart
+          const Text(
+            'The Question...',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            answerText: 'Answer text 1....',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer text 2....',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer text 3....',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
